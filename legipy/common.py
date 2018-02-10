fc1368b2b17f4690e9058d1d681078e54a928220
Laurent LAPORTE <tantale.solutions@gmail.com>
2018-02-10 23:57:22 +0100

    Fix some PEP8 warnings.
    Conflicts:
    	legipy/common.py
    	legipy/parsers/law_parser.py

--- legipy/common.py
+++ legipy/common.py
@@ -45,7 +45,7 @@ def page_url(page):
 
 
 def cleanup_url(url):
-    return re.sub(r';jsessionid=[^\?]+\?', '?', url)
+    return re.sub(r';jsessionid=[^?]+\?', '?', url)
 
 
 def merge_spaces(string):
@@ -61,6 +61,7 @@ def parse_date(string):
     try:
         month = 1 + MONTHS.index(match.group(2))
     except ValueError:
+        # not in list
         return None
 
     return datetime.date(int(match.group(3)), month, int(match.group(1)))
