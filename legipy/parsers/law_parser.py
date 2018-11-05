4952a042704b9a1835162cd089feaa2508acf87e
Benjamin Ooghe-Tabanou <benjamin.ooghe@sciences-po.fr>
2018-11-05 11:40:32 +0100

    fixing flake generates more flake warnings ;)

--- legipy/parsers/law_parser.py
+++ legipy/parsers/law_parser.py
@@ -34,8 +34,8 @@ def parse_law(url, html, id_legi):
         law.number = law_num.group(2)
         title_remain = law_num.group(3)
 
-    prop = re.match(r'(proj|prop)(?:et de loi|osition de loi) (\w+)', law.title,
-                    re.I)
+    prop = re.match(r'(proj|prop)(?:et de loi|osition de loi) (\w+)',
+                    law.title, re.I)
     if prop:
         law.type = prop.group(1).lower()
 
